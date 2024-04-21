b984d834db9a0c7f8e34b70e70cc53f74c062cac
Egor <Nesterovegor09@yandex.ru>
2024-04-21 18:05:05 +0400

    added in ex.1

--- HTP/HomeWork_2/Exersice_1/main.cpp
+++ HTP/HomeWork_2/Exersice_1/main.cpp
@@ -31,6 +31,8 @@ struct AbsToken {};
 
 struct MaxToken {};
 
+struct Comma {};
+
 struct Plus {};
 
 struct Minus {};
@@ -43,17 +45,51 @@ struct Divide {};
 
 struct SqrtToken {};
 
-using Token = std::variant<OpeningBracket, ClosingBracket, Number, UnknownToken, MinToken, AbsToken, MaxToken, Plus, Minus, Multiply, Modulo, Divide, SqrtToken>;
+using TokenBase = std::variant<OpeningBracket, ClosingBracket, Number, UnknownToken, MinToken, AbsToken, MaxToken, Plus, Minus, Multiply, Modulo, Divide, SqrtToken, Comma>;
 
-// 1234
+struct Token : TokenBase {
+    using TokenBase::TokenBase;
+
+    template <typename T>
+    [[nodiscard]] bool Is() const {
+        return std::holds_alternative<T>(*this);
+    }
+
+    template <typename T>
+    [[nodiscard]] const T& As() const {
+        return std::get<T>(*this);
+    }
+
+    template <typename T>
+    [[nodiscard]] const T* TryAs() const {
+        return std::get_if<T>(this);
+    }
+};
 
 const std::unordered_map<char, Token> kSymbol2Token{
-    {'+', Plus{}}, {'-', Minus{}}, {'*', Multiply{}}, {'/', Divide{}}, {'%', Modulo{}}, {'(', OpeningBracket{}}, {')', ClosingBracket{}}
-};
+    {'+', Plus{}}, {'-', Minus{}}, {'*', Multiply{}}, {'/', Divide{}}, {'%', Modulo{}}, {'(', OpeningBracket{}}, 
+    {')', ClosingBracket{}}, {',', Comma{}} };
 
 const std::unordered_map<std::string, Token> String2Token{
-    {"min", MinToken{}}, {"max", MaxToken{}}, {"abs", AbsToken{}}, {"sqrt", SqrtToken{}}
-};
+    {"min", MinToken{}}, {"max", MaxToken{}}, {"abs", AbsToken{}}, {"sqrt", SqrtToken{}} };
+
+bool operator==(const Token& lhs, const Token& rhs) {
+
+    if (lhs.index() != rhs.index()) {
+        return false;
+    }
+    if (lhs.Is<Number>()) {
+        return lhs.As<Number>().value == rhs.As<Number>().value;
+    }
+    if (lhs.Is<Number>()) {
+        return lhs.As<UnknownToken>().value == rhs.As<UnknownToken>().value;
+    }
+    return true;
+}
+
+bool operator!=(const Token& lhs, const Token& rhs) {
+    return !(lhs == rhs);
+}
 
 int ToDigit(unsigned char symbol) {
   return symbol - '0';
@@ -65,7 +101,8 @@ Number ParseNumber(const std::string& input, size_t& pos) {
   while (std::isdigit(symbol)) {
     value = value * 10 + ToDigit(symbol);
     if (pos == input.size() - 1) {
-      break;
+        ++pos;
+        break;
     }
     symbol = static_cast<unsigned char>(input[++pos]);
   }
@@ -75,9 +112,10 @@ Number ParseNumber(const std::string& input, size_t& pos) {
 Token ParseName(const std::string& input, size_t& pos) { //min, max, abs, sqrt;
     std::string name;
     auto symbol = static_cast<unsigned char>(input[pos]);
-    while (std::isdigit(symbol)) {
+    while (std::isalpha(symbol)) {
         name += symbol;
         if (pos == input.size() - 1) {
+            ++pos;
             break;
         }
         symbol = static_cast<unsigned char>(input[++pos]);
@@ -95,23 +133,27 @@ std::vector<Token> Tokenize(const std::string& input) {
   while (pos < size) {
     const auto symbol = static_cast<unsigned char>(input[pos]);
     if (std::isspace(symbol)) {
-      ++pos;
+        ++pos;
     } else if (std::isdigit(symbol)) {
-      tokens.emplace_back(ParseNumber(input, pos));
-    } else if (auto it = kSymbol2Token.find(symbol); it != kSymbol2Token.end()) {
-      tokens.emplace_back(it->second);
-    }
-    else if (std::isalpha(symbol)) {
+        tokens.emplace_back(ParseNumber(input, pos));
+    } else if (std::isalpha(symbol)) {
         tokens.emplace_back(ParseName(input, pos));
+    } else if (auto it = kSymbol2Token.find(symbol); it != kSymbol2Token.end()) {
+        tokens.emplace_back(it->second);
+        ++pos;
     }
   }
   return tokens;
 }
 
 int main() {
-    //(1 + 2) + 3 * sqr(4) ->[(, 1, +, 2, ), +, 3, *, SqrToken, (, 4, )]
-    std::vector<Token> tokens = Tokenize("(1 + 2) ");
-    std::vector<Token> expected_tokens = { OpeningBracket{}, Number{1}, Plus{}, Number{2}, ClosingBracket{} };
+    //min(4, 1 + 16 / 2) + (1 + 2) + 3 * sqr(444) ->[min, (, 4, ',', 1, +, 16, *, 2, ), +, (, 1, +, 2, ), +, 3, *, SqrToken, (, 4, )]
+    std::string query = " min ( 4, 1 + 16 / 2 ) +(11   + 549832 )    + 3 *   sqrt   (444   ) ";
+    std::vector<Token> tokens = Tokenize(query);
+    std::vector<Token> expected_tokens = { MinToken{}, OpeningBracket{}, Number{4}, Comma{}, Number{1}, Plus{}, Number{16}, Divide{}, 
+                                            Number{2}, ClosingBracket{}, Plus{}, OpeningBracket{},  Number{11}, Plus{}, Number{549832}, 
+                                            ClosingBracket{}, Plus{}, Number{3}, Multiply{}, SqrtToken{}, OpeningBracket{}, Number{444}, 
+                                            ClosingBracket{} };
     assert(tokens == expected_tokens);
     std::cout << "Comlete" << std::endl;
 }
