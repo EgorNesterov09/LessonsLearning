c2ee32b32077a77b74e762ca7d73ba70cf9902e7
Egor <Nesterovegor09@yandex.ru>
2024-04-24 23:31:48 +0400

    ex.3

---
+++ HTP/Final_TEST/3/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    int* ptr = &arr[0];
+
+    // Используем арифметику указателей для доступа к четвертому элементу
+    int fourthElement = *(ptr + 3);
+
+    std::cout << "4-й элемент массива: " << fourthElement << std::endl;
+
+    return 0;
+}
